56a04b0aaff63189f0b8e29fb3e289ab2cb8f760
koojunho <kneadtruth@gmail.com>
2022-07-19 00:51:19 +0900

    wip

--- gostock/utils/StockUtil.py
+++ gostock/utils/StockUtil.py
@@ -13,3 +13,52 @@ class StockUtil:
         if not data:
             return None
         return data[0]
+
+    @staticmethod
+    def get_hoga_gap(begin_price, end_price, market):
+        gaps = [
+            {'min': 0, 'unit': 1},
+            {'min': 1000, 'unit': 5},
+            {'min': 5000, 'unit': 10},
+            {'min': 10000, 'unit': 50},
+            {'min': 50000, 'unit': 100}
+        ]
+        if market == StockUtil.MARKET_KOSPI:
+            gaps += [{'min': 100000, 'unit': 500}, {'min': 500000, 'unit': 1000}]
+        elif market == StockUtil.MARKET_KOSDAQ:
+            pass
+        else:
+            raise Exception(f'알 수 없는 마켓: {market}')
+
+        if begin_price < end_price:
+            sign = 1
+            temp_begin_price = begin_price
+            temp_end_price = end_price
+        else:
+            sign = -1
+            temp_begin_price = end_price
+            temp_end_price = begin_price
+
+        begin_gap_idx = -1
+        end_gap_idx = -1
+        for idx, gap in enumerate(gaps):
+            if gap['min'] <= temp_begin_price:
+                begin_gap_idx = idx
+            if gap['min'] <= temp_end_price:
+                end_gap_idx = idx
+
+        n_gap = 0
+        for idx in range(begin_gap_idx, end_gap_idx + 1):
+            unit = gaps[idx]['unit']
+            if idx == begin_gap_idx:
+                left = temp_begin_price
+            else:
+                left = gaps[idx]['min']
+            if idx == end_gap_idx:
+                right = temp_end_price
+            else:
+                right = gaps[idx + 1]['min']
+            n_gap += int((right - left) / unit)
+        n_gap = sign * n_gap
+
+        return n_gap
