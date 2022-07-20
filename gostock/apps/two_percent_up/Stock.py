83aa4305bc90742d6ba098f5dde137f65e2d26db
koojunho <kneadtruth@gmail.com>
2022-07-21 08:07:03 +0900

    wip

--- gostock/apps/two_percent_up/Stock.py
+++ gostock/apps/two_percent_up/Stock.py
@@ -4,7 +4,7 @@ from gostock.utils import *
 
 
 class Stock:
-    MAX_SEC = 50
+    MAX_SEC = 10
     SIZE_OF_PRICE_HISTORY = MAX_SEC
 
     def __init__(self, code, name, market):
@@ -46,14 +46,21 @@ class Stock:
 
         for ph in reversed(self.price_history):
             # 주문이 적은데 급등이면 피한다
-            if self.order_per_sec_last < 10:
+            if self.order_per_sec_last < 20:
                 break
+
             # MAX_SEC 초 이내의 급등을 찾아야 하므로 MAX_SEC 초를 넘어가면 종료
             if curr_time - ph.time > self.MAX_SEC:
                 break
-            # 급등을 찾아야 하므로 급등률이 2%보다 작으면 종료
-            if curr_rate - ph.rate < 1:
+
+            # # 급등을 찾아야 하므로 급등률이 1%보다 작으면 종료
+            # if curr_rate - ph.rate < 1:
+            #     break
+
+            # 급등을 찾아야 하므로 호가 갭이 4보다 작으면 종료
+            if StockUtil.get_hoga_gap(ph.price, curr_price, self.market) < 4:
                 break
+
             self.soar = True
             self.soar_rate = curr_rate - ph.rate
             self.price_history = []
