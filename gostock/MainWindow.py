f696410b508bffcab13e435b1ac249bf028994b2
koojunho <kneadtruth@gmail.com>
2022-07-23 02:56:24 +0900

    Always on top도 세팅에 저장

--- gostock/MainWindow.py
+++ gostock/MainWindow.py
@@ -15,9 +15,16 @@ class MainWindow(QMainWindow):
             self.restoreGeometry(self.settings.value("geometry"))
         if self.settings.value("windowState"):
             self.restoreState(self.settings.value("windowState"))
+        self.always_on_top = False
+        aot = self.settings.value("alwaysOnTop")
+        if aot and aot == 'true':
+            self.always_on_top = True
+
+        # 윈도우
 
         self.setWindowTitle('GoStock Final')
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        if self.always_on_top:
+            self.setWindowFlags(Qt.WindowStaysOnTopHint)
 
         self.kiwoom = MyKiwoom()
 
@@ -31,6 +38,7 @@ class MainWindow(QMainWindow):
 
         file_menu = bar.addMenu("파일(&F)")
         apps_menu = bar.addMenu("앱(&A)")
+        window_menu = bar.addMenu("창(&W)")
 
         action = file_menu.addAction("로그인(&L)")
         action.setShortcut(QKeySequence("Ctrl+L"))
@@ -46,6 +54,13 @@ class MainWindow(QMainWindow):
         self.check_widget = None
         action.triggered.connect(self.show_check_two_in_five_widget)
 
+        action = window_menu.addAction("Always on top")
+        action.setShortcut(QKeySequence("Ctrl+T"))
+        action.setCheckable(True)
+        if self.always_on_top:
+            action.setChecked(True)
+        action.triggered.connect(self.always_on_top_menu_selected)
+
         self.kiwoom_timer = QTimer()
         self.kiwoom_timer.setInterval(100)
         self.kiwoom_timer.timeout.connect(self.kiwoom_timer_handler)
@@ -56,6 +71,7 @@ class MainWindow(QMainWindow):
     def closeEvent(self, event):
         self.settings.setValue("geometry", self.saveGeometry())
         self.settings.setValue("windowState", self.saveState())
+        self.settings.setValue("alwaysOnTop", self.always_on_top)
         QMainWindow.closeEvent(self, event)
 
     def kiwoom_timer_handler(self):
@@ -88,3 +104,13 @@ class MainWindow(QMainWindow):
             return True
         else:
             return False
+
+    def always_on_top_menu_selected(self, action):
+        self.always_on_top = action
+        # https://stackoverflow.com/a/33025835 flags 변경 후에 윈도우가 감쳐지는데 show()를 호출하라고 한다.
+        if self.always_on_top:
+            self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+            self.show()
+        else:
+            self.setWindowFlags(self.windowFlags() & ~Qt.WindowStaysOnTopHint)
+            self.show()
